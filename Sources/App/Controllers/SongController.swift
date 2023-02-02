109f98e44fe638356594fa7cf7cfa58adf1cff99
lulu <lulukalmisfer@hotmail.com>
2023-02-02 12:37:56 +0300

    add PUT and DELETE req (p1)

--- Sources/App/Controllers/SongController.swift
+++ Sources/App/Controllers/SongController.swift
@@ -13,6 +13,10 @@ struct SongController: RouteCollection {
         let songs = routes.grouped("songs")
         songs.get(use: index)
         songs.post(use: create)
+        songs.put(use: update)
+        songs.group(":songID") { song in
+                 song.delete(use: delete)
+             }
     }
     
     
@@ -25,5 +29,25 @@ struct SongController: RouteCollection {
     func create(req: Request) throws -> EventLoopFuture<HTTPStatus> {
         let song = try req.content.decode(Song.self)
         return song.save(on: req.db).transform(to: .ok)
+    }
+    
+    // PUT Request /songs routes
+    func update(req: Request) throws -> EventLoopFuture<HTTPStatus> {
+        let song = try req.content.decode(Song.self)
+        
+        return Song.find(song.id, on: req.db)
+        .unwrap(or: Abort(.notFound))
+        .flatMap {
+            $0.title = song.title
+            return $0.update(on: req.db).transform(to: .ok)
+        }
+    }
+    
+    // DELETE Request /songs/id route
+    func delete(req: Request) throws -> EventLoopFuture<HTTPStatus> {
+        Song.find(req.parameters.get("songID"), on: req.db)
+            .unwrap(or: Abort(.notFound))
+            .flatMap { $0.delete(on: req.db) }
+            .transform(to: .ok)
     }
 }
